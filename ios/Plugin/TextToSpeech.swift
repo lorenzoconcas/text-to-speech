6324bb30a55d1b750e528c5b5b60737b68c312ba
Lorenzo Concas <35010999+lorenzoconcas@users.noreply.github.com>
2025-05-26 16:39:12 +0200

    Update TextToSpeech.swift

--- ios/Plugin/TextToSpeech.swift
+++ ios/Plugin/TextToSpeech.swift
@@ -47,6 +47,7 @@ enum QUEUE_STRATEGY: Int {
     }
 
     public func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        plugin?.notifyListeners("onDone", data: [:])   
         self.resolveCurrentCall()
     }
 
